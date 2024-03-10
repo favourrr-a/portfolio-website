1cfee22c2d5030bf0d2ae8cb17c43050d8833be0
nerdfavour <favourackonu13@gmail.com>
2024-03-10 21:11:34 +0000

    added three categories for the header content

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -3,11 +3,34 @@ function HeaderContent(){
     return(
     <>
     <div id="header-content-1">
-    <h2> Hi, I'm Favour</h2>
+    
 
     </div>
     <div id="header-content-2">
-<p>Yo!</p>
+        <div className='header-actual-content'>
+        <h2> Hi, I'm Favour</h2>
+        <div>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Vivamus arcu felis bibendum ut tristique et. Faucibus a pellentesque sit amet porttitor eget dolor. At elementum eu facilisis sed 
+            odio morbi quis. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Neque ornare aenean euismod elementum. Purus 
+            sit amet volutpat consequat mauris. Vitae 
+            semper quis lectus nulla at volutpat diam ut. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Massa tincidunt nunc pulvinar sapien et.
+            </p>
+        </div>
+        <div>
+            <div className='buttons'>
+                <div>
+hire me
+                </div>
+                <div>
+see stuff i've done
+                </div>
+            </div>
+        </div>
+        </div>
+    
+        
     </div>
     </>
     )
