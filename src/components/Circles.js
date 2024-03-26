c8e61bbee20a00a85134fdce2904303228d7c602
nerdfavour <favourackonu13@gmail.com>
2024-03-26 11:21:21 +0000

    made circles change to a random color when clicked

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -1,12 +1,19 @@
 import '../styles/Circles.css'
-function Circles(){
-    return(
-        <>
-        <div className = "circle">
-        </div>
-        </>
-        
-    )
+import './functions'
+import React, { useState } from 'react';
+
+function Circle() {
+  const colors = ['#ff6347', '#4682b4', '#90ee90', '#ffa500', '#8a2be2'];
+  const [color, setColor] = useState('#ff6347'); // Default color
+
+  const changeColor = () => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+  };
+
+  return (
+    <div className="circle" style={{ backgroundColor: color }} onClick={changeColor}></div>
+  );
 }
 
-export default Circles;
+export default Circle;
--- src/components/HeaderBackground.js
+++ src/components/HeaderBackground.js
@@ -2,7 +2,13 @@ import Circles from "./Circles";
 import '../styles/HeaderBackground.css'
 function HeaderBackground(){
     return(
+    <>
+    <div className="headerBackground"><Circles/></div>
         <div className="headerBackground"><Circles/></div>
+        <div className="headerBackground"><Circles/></div>
+    </>
+        
+
     )
 }
 
