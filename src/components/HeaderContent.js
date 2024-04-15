be0606cd923aa722140dbc5a125b9795c4222686
nerdfavour <favourackonu13@gmail.com>
2024-04-15 21:05:41 +0000

    begun with positioning of image on page

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,4 +1,5 @@
 import '../styles/HeaderContent.css'
+import image from './Image';
 import { useNavigate } from 'react-router-dom';
 
 function HeaderContent(){
@@ -15,25 +16,28 @@ function HeaderContent(){
     </div>
     <div id="header-content-2">
         <div className='header-actual-content'>
-        <h2> Hi, I'm Favour</h2>
-        <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Vivamus arcu felis bibendum ut tristique et. Faucibus a pellentesque sit amet porttitor eget dolor. At elementum eu facilisis sed 
-            odio morbi quis. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Neque ornare aenean euismod elementum. Purus 
-            sit amet volutpat consequat mauris. Vitae 
-            semper quis lectus nulla at volutpat diam ut. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Massa tincidunt nunc pulvinar sapien et.
-            </p>
-        </div>
-        <div>
-            <div className='buttons'>
-                <div className='button-in-body'>
+            <h2> Hi, I'm Favour</h2>
+            <div>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Vivamus arcu felis bibendum ut tristique et. Faucibus a pellentesque sit amet porttitor eget dolor. At elementum eu facilisis sed 
+                odio morbi quis. Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Neque ornare aenean euismod elementum. Purus 
+                sit amet volutpat consequat mauris. Vitae 
+                semper quis lectus nulla at volutpat diam ut. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Massa tincidunt nunc pulvinar sapien et.
+                </p>
+            </div>
+            <div>
+                <div className='buttons'>
+                    <div className='button-in-body'>
 hire me
-                </div>
+                    </div>
                 <div className='button-in-body' onClick={navToThingsIveDone}>
 see stuff i've done
                 </div>
             </div>
+        </div>
+        <div className="image-container">
+            <image/>
         </div>
         </div>
     
