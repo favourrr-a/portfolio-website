7f0fcc4036489af2a0f5e97883c1e190a67d0d01
nerdfavour <favourackonu13@gmail.com>
2024-04-15 21:05:03 +0000

    added a placeholder image to use

---
+++ src/components/Image.js
@@ -0,0 +1,10 @@
+import imageOfMe from "../assets/images/placehoder-image.jpeg"
+import '../styles/Image.css'
+function image(){
+    return(
+        <>
+        <image src = {imageOfMe}/></>
+    )
+}
+
+export default image;
