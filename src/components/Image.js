3e4fd9451cef92e5391a0e583263407b02cde6c6
nerdfavour <favourackonu13@gmail.com>
2024-04-16 20:13:37 +0000

    fixed image not showing i used 'image' instead of 'img' so it wasn't recognized. fixed that

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,9 +2,7 @@ import imageOfMe from "../assets/images/placehoder-image.jpeg"
 import '../styles/Image.css'
 function Image(){
     return(
-        <>
-        <image src = {imageOfMe}/>
-        </>
+        <img src = {imageOfMe}/>
     )
 }
 
