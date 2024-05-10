ee6f8e1c1d66f51ff8e74f70b25ea8afc6356481
nerdfavour <favourackonu13@gmail.com>
2024-05-10 13:28:47 +0000

    updated linkedIn link

--- src/components/SocialMediaLinks.js
+++ src/components/SocialMediaLinks.js
@@ -6,7 +6,7 @@ import {FaX} from 'react-icons/fa6';
 function SocialMediaLinks(){
     return(
 <div className='links'>
-                <a href = "https://www.linkedin.com/in/favour-ackonu-b046a3295/">
+                <a href = "https://www.linkedin.com/in/favour-ackonu/">
                     <FaLinkedin className = "link-in-body"/>
                 </a>
                 <a href = "https://github.com/favourrr-a">
