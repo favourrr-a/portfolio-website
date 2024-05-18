df840c4f933d1da097f58f843569b644ef678730
nerdfavour <favourackonu13@gmail.com>
2024-05-18 19:10:39 +0000

    trying to fix routing problem without HashRouter HashRouter looks terrible and i'd have to amend some of my code to accomodate it. that'd be a drag so i'm trying the 404.html method

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import BackgroundParticles from './components/BackgroundParticles';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <BackgroundParticles/>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
