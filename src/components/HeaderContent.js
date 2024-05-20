9353f5406a8848b41e6f53fe5531851dfa3c1287
nerdfavour <favourackonu13@gmail.com>
2024-05-20 20:47:29 +0000

    fixed grammatical errors in HeaderContent paragraph

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -26,11 +26,11 @@ function HeaderContent(){
             <TextAnimation par={!smallScreen ? "text-animation" : "text-animation-small"}/>
             <div>
                 <p className = {!smallScreen ? "paragraph" : "paragraph-small"}>
-                    My name is Favour Ackonu. I am student of University of Ghana studying Bsc. Mathematical
+                    My name is Favour Ackonu. I am a student of University of Ghana studying Bsc. Mathematical
                     Science, Computer Science with Mathematics. I am 18 years old. I am currently interested in
-                    anything relating computers, and have not yet chosen a specific field to specify in. I do know what
-                    I do not want to specialize in, and that is Front-End development. It's too hard for me. I mean, this website
-                    is literally my best frontend work, and look at the state of it. But yes, I am willing to learn new things.
+                    anything related to computers, and I have not yet chosen a specific field to specialize in. However, I do know what
+                    I do not want to specialize in, and that is front-end development. It's too difficult for me. I mean, this website
+                    is literally my best front-end work, and look at the state of it. But yes, I am willing to learn new things.
                 </p>
             </div>
             <SocialMediaLinks/>
